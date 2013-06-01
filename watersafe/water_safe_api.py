0ea38fe2716f529386aec973e56429d45d589882
Javon Harper <javon.d.harper@gmail.com>
2013-06-01 17:50:30 -0400

    Add scraper to find county based off zip

--- watersafe/water_safe_api.py
+++ watersafe/water_safe_api.py
@@ -6,7 +6,24 @@ import urllib
 import httplib2
 import json
 
+from bs4 import BeautifulSoup
 http = httplib2.Http()
+
+# Scrape http://zip-info.com to get county info
+def get_county_by_zip(zip):
+  url = "http://www.zip-info.com/cgi-local/zipsrch.exe?cnty=cnty&zip={0}&Go=Go".format(zip)
+  response = do_GET(url)
+  soup = BeautifulSoup(response)
+
+  # There's a handful of tables in the page, the one we care about is the third one.
+  results_table_index = 3
+  results_table = soup.find_all('table')[results_table_index]
+
+  # 4th column has the county code
+  county_column_index = 4
+  county_code_tag = results_table.find_all('td')[county_column_index]
+  county_code = county_code_tag.contents
+  return county_code
 
 def do_GET(url):
   body = {}
@@ -24,10 +41,16 @@ def get_violations_by_pws(pwsid):
   response = do_GET(url)
   return json.loads(response)
 
-philly_county_code = 42101
-water_systems = get_pws_by_county(philly_county_code)
+## Debug stuffs
 
-for water_system in water_systems:
-  pwsid = water_system['PWSID']
-  violations = get_violations_by_pws(pwsid) 
-  print violations
+# Test getting county by zip
+print get_county_by_zip(19131)
+
+# Test getting violations by county
+# philly_county_code = 42101
+# water_systems = get_pws_by_county(philly_county_code)
+
+# for water_system in water_systems:
+#   pwsid = water_system['PWSID']
+#   violations = get_violations_by_pws(pwsid) 
+#   print violations
