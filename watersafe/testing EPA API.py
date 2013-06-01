1349684521994be002953d16c7cad72d5b2087d7
Javon Harper <javon.d.harper@gmail.com>
2013-06-01 14:32:27 -0400

    Get pws for county codes

--- watersafe/testing EPA API.py
+++ watersafe/testing EPA API.py
@@ -7,9 +7,16 @@ import httplib2
 
 http = httplib2.Http()
 
-url = "http://iaspub.epa.gov/enviro/efservice/tri_facility/state_abbr/VA/rows/499:504/json"
-body = {}
-headers = {'Content-type': 'application/x-www-form-urlencoded'}
-response, content = http.request(url, 'GET', headers=headers, body=urllib.urlencode(body))
+def request_url(url):
+  body = {}
+  headers = {'Content-type': 'application/x-www-form-urlencoded'}
+  response, content = http.request(url, 'GET', headers=headers, body=urllib.urlencode(body))
+  return content
 
-print content
+def get_pws_for_county(county_code):
+  url =  "http://iaspub.epa.gov/enviro/efservice/PWS_COUNTY/fipscounty/{0}/json".format(42101)
+  return request_url(url)
+
+philly_county_code = 42101
+response = get_pws_for_county(philly_county_code)
+print response
